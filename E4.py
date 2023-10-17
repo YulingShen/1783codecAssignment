4217857f15c6f522e0d7bccdb6719963e9d966cb
YulingShen <yulingshen0326@gmail.com>
2023-10-17 02:12:11 -0400

    adding sample using mother_daughter

--- E4.py
+++ E4.py
@@ -2,7 +2,7 @@ from codec import E4process
 from utils import reader
 
 if __name__ == '__main__':
-    filepath = './files/foreman_cif_y.yuv'
+    filepath = './files/mother_daughter_y.yuv'
     w = 352
     h = 288
     i = 8
@@ -10,11 +10,11 @@ if __name__ == '__main__':
     r = 4
     qp = 2
     period = 4
-    frames = 12
+    frames = 32
     # E4process.encode_tran_quan(filepath, w, h, i, n, r, qp, frames)
     # reader.res_abs('./files/foreman_cif_y_res.yuv')
     # E4process.encode_intra(filepath, w, h, i, n, qp, frames)
     # E4process.encode_intra_period(filepath, w, h, i, n, r, qp, period, frames)
     # E4process.decode_intra_period(filepath, w, h, i, qp, period)
-    # E4process.encode_intra_period_entropy(filepath, w, h, i, n, r, qp, period, frames)
+    E4process.encode_intra_period_entropy(filepath, w, h, i, n, r, qp, period, frames)
     E4process.decode_intra_period_entropy(filepath, w, h, i, n, r, qp, period)
