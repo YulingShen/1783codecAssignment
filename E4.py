62d9ee4b19bc89841c1ccce996385a24e4a0b90b
YulingShen <yulingshen0326@gmail.com>
2023-10-29 02:18:53 -0400

    A1 modification for reports

--- E4.py
+++ E4.py
@@ -11,10 +11,5 @@ if __name__ == '__main__':
     qp = 7
     period = 4
     frames = 10
-    # E4process.encode_tran_quan(filepath, w, h, i, n, r, qp, frames)
-    # reader.res_abs('./files/foreman_cif_y_res.yuv')
-    # E4process.encode_intra(filepath, w, h, i, n, qp, frames)
-    # E4process.encode_intra_period(filepath, w, h, i, n, r, qp, period, frames)
-    # E4process.decode_intra_period(filepath, w, h, i, qp, period)
     E4process.encode_complete(filepath, w, h, i, n, r, qp, period, frames)
     E4process.decode_complete(filepath)
